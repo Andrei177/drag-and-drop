97cb2f0a08e9a1e73a728cddc524fc2c6cc900fa
Andrey <andrejlapin4594@gmail.com>
2024-02-09 16:20:41 +0500

    Добавил basename

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/drag-and-drop/'>
       <AppRouter/>
     </BrowserRouter>
   )
